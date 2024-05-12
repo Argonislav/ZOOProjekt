062ebd110e6bb3d7b9d61df3d9400ece03d73a5f
lukasek15947 <houskal.04@spst.eu>
2024-05-12 15:37:37 +0200

    Vytvoření třídy Game

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lukas Houska on 12.05.2024.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by Lukas Houska on 12.05.2024.
+//
+
+#ifndef ZOOPROJEKT_GAME_H
+#define ZOOPROJEKT_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //ZOOPROJEKT_GAME_H
