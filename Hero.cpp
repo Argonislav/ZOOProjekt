17896df28efad5140b011401eeeeef7b0f40b2a4
lukasek15947 <houskal.04@spst.eu>
2024-05-12 17:22:01 +0200

    Deklarování metod v třídě Hero - Jimmy

---
+++ Hero.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Jindřich Šantrůček on 12.05.2024.
+//
+
+#include "Hero.h"
+
+Hero::Hero(std::string name, float health, float attack, float defense)
+        : Character(name, health, attack, defense) {}
