1512e7d0ba2d3e1ddb5673e70a2e567fc3562771
lukasek15947 <houskal.04@spst.eu>
2024-05-12 17:22:59 +0200

    Test tříd Monster a Hero - Jimmy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "Hero.h"
+#include "Monster.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Hero* hero= new Hero("Brave Knight", 100, 20, 10);
+    Monster* monster = new Monster("Evil Goblin", 50, 15, 5);
+
+    hero->attack(monster);
+    monster->printInfo();
+
+    delete hero;
+    delete monster;
     return 0;
 }
