1f6e20b75e3f3fa05102ebb18ab41377d27c0f19
lukasek15947 <houskal.04@spst.eu>
2024-05-12 16:23:05 +0200

    Definování základní struktury třídy Item

--- Item.h
+++ Item.h
@@ -22,6 +22,29 @@ private:
     int attackBonus;
     int healthBonus;
     int defenseBonus;
+
+public:
+    Item(const std::string& name, const std::string& description, ItemType type, int attackBonus = 0, int healthBonus = 0, int defenseBonus = 0);
+
+    std::string getName();
+    void setName(std::string name);
+
+    std::string getDescription();
+    void setDescription(std::string description);
+
+    ItemType getType();
+    void setType(ItemType type);
+
+    int getAttackBonus();
+    void setAttackBonus(int attackBonus);
+
+    int getHealthBonus();
+    void setHealthBonus(int healthBonus);
+
+    int getDefenseBonus();
+    void setDefenseBonus(int defenseBonus);
+
+    virtual void use() = 0;
 };
 
 #endif //ZOOPROJEKT_ITEM_H
