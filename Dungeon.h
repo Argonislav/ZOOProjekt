dde68ad0f2b2fa405dbd3fa118c3fedbfeffee67
Argonislav <gregro.03@spst.eu>
2024-05-15 16:52:57 +0200

    vložen hlavičkový soubor Dungeon

--- Dungeon.h
+++ Dungeon.h
@@ -2,13 +2,34 @@
 // Created by matla on 5/15/2024.
 //
 
-#ifndef ZOOPROJEKT_DUNGEON_H
-#define ZOOPROJEKT_DUNGEON_H
+#ifndef DUNGEON_H
+#define DUNGEON_H
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 
-class Dungeon {
+enum class TileType { EMPTY, WALL, DOOR, PLAYER, OBSTACLE };
 
+
+struct Tile {
+    TileType type;
 };
 
+class Dungeon {
+private:
+    std::vector<std::vector<Tile>> layout;
+    int playerX, playerY;
+    int m_height, m_width;
+    int roomCount;
 
-#endif //ZOOPROJEKT_DUNGEON_H
+public:
+    Dungeon(int width, int height);
+    void generateRoom();
+    bool movePlayer(char direction);
+    void print() const;
+};
+
+#endif // ZOOPROJEKT_DUNGEON_H
