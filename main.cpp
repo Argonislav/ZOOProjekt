b473fdfd11dfe0891f06f4bf68716c002104753d
Argonislav <gregro.03@spst.eu>
2024-05-15 16:58:36 +0200

    vlozena funkce main, ve ktere se lze pohybovat

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-#include "Hero.h"
-#include "Monster.h"
+#include "Dungeon.h"
 
 int main() {
-    Hero* hero= new Hero("Brave Knight", 100, 20, 10);
-    Monster* monster = new Monster("Evil Goblin", 50, 15, 5);
+    Dungeon dungeon(10, 7);
+    dungeon.print();
 
-    hero->attack(monster);
-    monster->printInfo();
+    char direction;
+    while (true) {
+        std::cout << "Enter a key to move (or 'q' to quit): ";
+        std::cin >> direction;
+        if (direction == 'q') {
+            break;
+        }
+        if (dungeon.movePlayer(direction)) { // Kontrola ukončení hry
+            break;
+        }
+        std::cout << std::endl;
+        dungeon.print();
+    }
 
-    delete hero;
-    delete monster;
     return 0;
 }
