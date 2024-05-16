ce1240a72a5e5699de6f74f0e9f65a2091cf613d
lukasek15947 <houskal.04@spst.eu>
2024-05-16 17:05:46 +0200

    Dokončení smyčky - jimmy

--- main.cpp
+++ main.cpp
@@ -1,23 +1,47 @@
 #include <iostream>
 #include "Dungeon.h"
 
-int main() {
-    Dungeon dungeon(10, 7);
-    dungeon.print();
+using namespace std;
 
-    char direction;
-    while (true) {
-        std::cout << "Enter a key to move (or 'q' to quit): ";
-        std::cin >> direction;
-        if (direction == 'q') {
-            break;
+int main() {
+    int choice;
+
+    do {
+        cout << "======== Dungeon Game ========" << endl;
+        cout << "1. New game" << endl;
+        cout << "2. End" << endl;
+        cout << "Choose option:";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                // Spuštění hry v dungeonu
+                Dungeon dungeon(10, 7);
+                dungeon.print();
+
+                char direction;
+                while (true) {
+                    cout << "Enter a key to move (or 'q' to quit): ";
+                    cin >> direction;
+                    if (direction == 'q') {
+                        break;
+                    }
+                    if (dungeon.movePlayer(direction)) { // Kontrola ukončení hry
+                        break;
+                    }
+                    cout << endl;
+                    dungeon.print();
+                }
+                break;
+            }
+            case 2:
+                cout << "Game was terminated. Goodbye!" << endl;
+                return 0;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
         }
-        if (dungeon.movePlayer(direction)) { // Kontrola ukončení hry
-            break;
-        }
-        std::cout << std::endl;
-        dungeon.print();
-    }
+
+    } while (choice != 2);
 
     return 0;
 }
